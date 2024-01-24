2436b780f17576a21a3117a45247b423f4dac56a
FtKuBo <semmarmehdi558@gmail.com>
2024-01-24 01:46:36 -0500

    adding tasks option

--- src/App.jsx
+++ src/App.jsx
@@ -3,32 +3,40 @@ import { useState} from "react"
 
 export default function App(){
   const [newItem, setNewItem] = useState("")
+  const [todos, setTodos] = useState([])
+
+  function handleSubmit(e){
+    e.preventDefault()
+
+    setTodos(currentTodos =>{
+      return [ ...currentTodos,{id:crypto.randomUUID(), title:newItem,completed:false},]
+    })
+    
+    setNewItem("");
+  }
 
   return (
   <>
-  <form className = "form">
+  <form onSubmit={handleSubmit} className = "form">
     <div className = "formRow">
       <label htmlFor="textLaber">Type in your task: </label>
-      <input type="text" id="item" />
+      <input value={newItem} onChange={e => setNewItem(e.target.value)} type="text" id="item" />
     </div>
-    <button className="btn">Add</button>
+    <button className="btn">Add</button>  
   </form>
   <h1 className="header">Todo List</h1>
   <ul className="list">
-    <li>
+   {todos.map(todo =>{
+    return (
+    <li key={todo.id}>
       <label>
-        <input type="checkbox"/>
-          Item1
+        <input type="checkbox" checked={todo.completed}/>
+        {todo.title}
       </label>
       <button className="btn btn-danger">Delete</button>
     </li>
-    <li>
-      <label>
-        <input type="checkbox"/>
-          Item2 
-      </label>
-      <button className="btn btn-danger">Delete</button>
-    </li>
+    )
+   })}
   </ul>
   </>
   )
